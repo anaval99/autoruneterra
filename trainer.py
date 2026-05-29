97d0b78b16ce1dc89034501fa0247113c883d842
anaval99 <arnold.naval99@proton.me>
2026-05-28 18:24:32 -0600

    lower lr and more darius data

--- trainer.py
+++ trainer.py
@@ -201,7 +201,7 @@ def build_model():
 
 
 #####  train model  ################################################################
-def train_model(model, train_loader, val_loader, device, save_path, epochs=10, lr=1/10000):
+def train_model(model, train_loader, val_loader, device, save_path, epochs=10, lr=1/30000):
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
     criterion = nn.MSELoss()
 
